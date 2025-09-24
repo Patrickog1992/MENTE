eec20c7fa03a2fe6c252ba91270ec85198885c7f
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-24 19:52:31 +0000

    https://i.imgur.com/Xn7zzBZ.png coloque essa logo no topo do site centra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,19 @@ import { Guarantee } from "@/components/sections/guarantee";
 import { Faq } from "@/components/sections/faq";
 import { Footer } from "@/components/sections/footer";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
+      <header className="py-6 flex justify-center">
+        <Image 
+          src="https://i.imgur.com/Xn7zzBZ.png"
+          alt="Mente Milionária Logo"
+          width={100}
+          height={100}
+        />
+      </header>
       <main className="flex-1">
         <Hero />
         <section className="py-12 sm:py-20 bg-background">
